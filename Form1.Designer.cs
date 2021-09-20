ab47e091a6457ecc1b7837006cca288c3265a730
jaimenunes <74732199+jaimenunes@users.noreply.github.com>
2021-09-20 13:30:32 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,315 @@
+﻿
+namespace ControleEstoque
+{
+    partial class FormControle
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbNumTam = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbAlfaTam = new System.Windows.Forms.ComboBox();
+            this.tbProduto = new System.Windows.Forms.TextBox();
+            this.tbCor = new System.Windows.Forms.TextBox();
+            this.tbQtd = new System.Windows.Forms.NumericUpDown();
+            this.gridEstoque = new System.Windows.Forms.DataGridView();
+            this.colID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colProduto = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colSexo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colTamanho = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colCor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colQuantidade = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnInserir = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cbSexo = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.tbNumTam)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbQtd)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridEstoque)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(44, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Produto";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(588, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tamanho";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(942, 56);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Cor";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(1118, 59);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "QTD";
+            // 
+            // tbNumTam
+            // 
+            this.tbNumTam.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbNumTam.Location = new System.Drawing.Point(718, 54);
+            this.tbNumTam.Name = "tbNumTam";
+            this.tbNumTam.Size = new System.Drawing.Size(43, 26);
+            this.tbNumTam.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(771, 56);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 25);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "OU";
+            // 
+            // tbAlfaTam
+            // 
+            this.tbAlfaTam.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbAlfaTam.FormattingEnabled = true;
+            this.tbAlfaTam.Items.AddRange(new object[] {
+            "",
+            "PP",
+            "P",
+            "M",
+            "G",
+            "GG",
+            "XG",
+            "EXG"});
+            this.tbAlfaTam.Location = new System.Drawing.Point(819, 53);
+            this.tbAlfaTam.Name = "tbAlfaTam";
+            this.tbAlfaTam.Size = new System.Drawing.Size(95, 28);
+            this.tbAlfaTam.TabIndex = 8;
+            // 
+            // tbProduto
+            // 
+            this.tbProduto.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbProduto.Location = new System.Drawing.Point(141, 48);
+            this.tbProduto.Multiline = true;
+            this.tbProduto.Name = "tbProduto";
+            this.tbProduto.Size = new System.Drawing.Size(232, 33);
+            this.tbProduto.TabIndex = 9;
+            this.tbProduto.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbProduto_KeyPress);
+            // 
+            // tbCor
+            // 
+            this.tbCor.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbCor.Location = new System.Drawing.Point(1004, 53);
+            this.tbCor.Name = "tbCor";
+            this.tbCor.Size = new System.Drawing.Size(95, 26);
+            this.tbCor.TabIndex = 10;
+            // 
+            // tbQtd
+            // 
+            this.tbQtd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbQtd.Location = new System.Drawing.Point(1184, 53);
+            this.tbQtd.Name = "tbQtd";
+            this.tbQtd.Size = new System.Drawing.Size(50, 26);
+            this.tbQtd.TabIndex = 11;
+            // 
+            // gridEstoque
+            // 
+            this.gridEstoque.BackgroundColor = System.Drawing.Color.White;
+            this.gridEstoque.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridEstoque.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colID,
+            this.colProduto,
+            this.colSexo,
+            this.colTamanho,
+            this.colCor,
+            this.colQuantidade});
+            this.gridEstoque.Location = new System.Drawing.Point(39, 218);
+            this.gridEstoque.Name = "gridEstoque";
+            this.gridEstoque.Size = new System.Drawing.Size(1215, 413);
+            this.gridEstoque.TabIndex = 12;
+            // 
+            // colID
+            // 
+            this.colID.HeaderText = "ID";
+            this.colID.Name = "colID";
+            // 
+            // colProduto
+            // 
+            this.colProduto.HeaderText = "Produto";
+            this.colProduto.Name = "colProduto";
+            this.colProduto.Width = 500;
+            // 
+            // colSexo
+            // 
+            this.colSexo.HeaderText = "Sexo";
+            this.colSexo.Name = "colSexo";
+            this.colSexo.Width = 150;
+            // 
+            // colTamanho
+            // 
+            this.colTamanho.HeaderText = "Tamanho";
+            this.colTamanho.Name = "colTamanho";
+            // 
+            // colCor
+            // 
+            this.colCor.HeaderText = "Cor";
+            this.colCor.Name = "colCor";
+            this.colCor.Width = 200;
+            // 
+            // colQuantidade
+            // 
+            this.colQuantidade.HeaderText = "Quantidade";
+            this.colQuantidade.Name = "colQuantidade";
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.Location = new System.Drawing.Point(200, 135);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(139, 52);
+            this.btnLimpar.TabIndex = 13;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnInserir
+            // 
+            this.btnInserir.BackColor = System.Drawing.Color.Green;
+            this.btnInserir.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnInserir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnInserir.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInserir.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnInserir.Location = new System.Drawing.Point(39, 134);
+            this.btnInserir.Name = "btnInserir";
+            this.btnInserir.Size = new System.Drawing.Size(119, 52);
+            this.btnInserir.TabIndex = 14;
+            this.btnInserir.Text = "Cadastrar";
+            this.btnInserir.UseVisualStyleBackColor = false;
+            this.btnInserir.Click += new System.EventHandler(this.btnInserir_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(391, 57);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(58, 25);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Sexo";
+            // 
+            // cbSexo
+            // 
+            this.cbSexo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbSexo.FormattingEnabled = true;
+            this.cbSexo.Items.AddRange(new object[] {
+            "Unissex",
+            "Masculino",
+            "Feminino"});
+            this.cbSexo.Location = new System.Drawing.Point(468, 53);
+            this.cbSexo.Name = "cbSexo";
+            this.cbSexo.Size = new System.Drawing.Size(95, 28);
+            this.cbSexo.TabIndex = 16;
+            // 
+            // FormControle
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1285, 643);
+            this.Controls.Add(this.cbSexo);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnInserir);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.gridEstoque);
+            this.Controls.Add(this.tbQtd);
+            this.Controls.Add(this.tbCor);
+            this.Controls.Add(this.tbProduto);
+            this.Controls.Add(this.tbAlfaTam);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tbNumTam);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.Name = "FormControle";
+            this.Text = "Controle Estoque";
+            this.Load += new System.EventHandler(this.FormControle_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.tbNumTam)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbQtd)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridEstoque)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown tbNumTam;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox tbAlfaTam;
+        private System.Windows.Forms.TextBox tbProduto;
+        private System.Windows.Forms.TextBox tbCor;
+        private System.Windows.Forms.NumericUpDown tbQtd;
+        private System.Windows.Forms.DataGridView gridEstoque;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnInserir;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cbSexo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colProduto;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colSexo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colTamanho;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colCor;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colQuantidade;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+
+namespace ControleEstoque
+{
+    public partial class FormControle : Form
+    {
+
+        public FormControle()
+        {
+            InitializeComponent();
+        }
+
+        DataSet ds = new DataSet();
+        private MySqlConnectionStringBuilder conexaoBanco()
+        {
+            MySqlConnectionStringBuilder conectionDB = new MySqlConnectionStringBuilder();
+            conectionDB.Server = "localhost";
+            conectionDB.Database = "estoque";
+            conectionDB.UserID = "root";
+            conectionDB.Password = "";
+            conectionDB.SslMode = 0;
+            return conectionDB;
+        }
+
+        private void FormControle_Load(object sender, EventArgs e)
+        {
+            atualizar();
+
+            tbAlfaTam.SelectedIndex = 0;
+            cbSexo.SelectedIndex = 0;
+        }
+
+        private void atualizar()
+        {
+            MySqlConnectionStringBuilder conectionDB = conexaoBanco();
+            MySqlConnection doConection = new MySqlConnection(conectionDB.ToString());
+            try
+            {
+                doConection.Open();
+
+                MySqlCommand commandMysql = doConection.CreateCommand();
+                commandMysql.CommandText = "SELECT * FROM produto WHERE ativoProduto = 1";
+                MySqlDataReader reader = commandMysql.ExecuteReader();
+
+                gridEstoque.Rows.Clear();
+
+                while (reader.Read())
+                {
+                    DataGridViewRow row = (DataGridViewRow)gridEstoque.Rows[0].Clone();
+                    row.Cells[0].Value = reader.GetInt32(0); //id
+                    row.Cells[1].Value = reader.GetString(1); //prouto
+                    row.Cells[2].Value = reader.GetString(2); //sexo
+                    row.Cells[3].Value = reader.GetString(3); //tamanho
+                    row.Cells[4].Value = reader.GetString(4); //cor
+                    row.Cells[5].Value = reader.GetInt32(5); //quantidade
+                    gridEstoque.Rows.Add(row); //Add linha na tabela    
+                }
+                
+                doConection.Close();
+
+
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("Can't open connetion");
+                Console.WriteLine(ex.Message);
+            }
+        }
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            limparCampos();
+        }
+        private void limparCampos()
+        {
+            tbProduto.Clear();
+            tbNumTam.Value = 0;
+            tbAlfaTam.SelectedIndex = 0;
+            tbCor.Clear();
+            tbQtd.Value = 0;
+            cbSexo.SelectedIndex = 0;
+        }
+
+        private void btnInserir_Click(object sender, EventArgs e)
+        {
+            MySqlConnectionStringBuilder conectionDB = conexaoBanco();
+            MySqlConnection doConection = new MySqlConnection(conectionDB.ToString());
+
+            string tamanho;
+
+            if (tbNumTam.Value > 0 && tbAlfaTam.SelectedIndex != 0)
+            {
+                MessageBox.Show("Valor de Tamanho incorreto");
+                tamanho = "";
+            }
+            else if (tbNumTam.Value > 0 && tbAlfaTam.SelectedIndex == 0 && tbProduto.Text != "" && tbCor.Text != "")
+            {
+                tamanho = Convert.ToString(tbNumTam.Value);
+                try
+                {
+                    doConection.Open();
+
+                    MySqlCommand commandMysql = doConection.CreateCommand();
+
+
+                    commandMysql.CommandText = "INSERT INTO produto (nomeProduto,sexoProduto,tamanhoProduto,corProduto, quantidadeProduto)" +
+                        "VALUES('" + tbProduto.Text + "','" + cbSexo.GetItemText(cbSexo.SelectedItem) + "','" + tamanho + "','" + tbCor.Text + "','" + Convert.ToInt16(tbQtd.Value) + "')";
+                    commandMysql.ExecuteNonQuery();
+                    doConection.Close();
+                    MessageBox.Show("Item cadastrado com sucesso");
+                    atualizar();
+                    limparCampos();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            else if (tbNumTam.Value == 0 && tbAlfaTam.GetItemText(tbAlfaTam.SelectedIndex) != "" && tbProduto.Text != "" && tbCor.Text != "")
+            {
+                tamanho = tbAlfaTam.GetItemText(tbAlfaTam.SelectedItem);
+                try
+                {
+                    doConection.Open();
+
+                    MySqlCommand commandMysql = doConection.CreateCommand();
+
+
+                    commandMysql.CommandText = "INSERT INTO produto (nomeProduto,sexoProduto,tamanhoProduto,corProduto, quantidadeProduto)" +
+                        "VALUES('" + tbProduto.Text + "','" + cbSexo.GetItemText(cbSexo.SelectedItem) + "','" + tamanho + "','" + tbCor.Text + "','" + Convert.ToInt16(tbQtd.Value) + "')";
+                    commandMysql.ExecuteNonQuery();
+                    doConection.Close();
+                    MessageBox.Show("Item cadastrado com sucesso");
+                    atualizar();
+                    limparCampos();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Preencha corretamente os campos");
+            }
+        }
+
+        private void tbProduto_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            string searchQuery = tbProduto.Text;
+            MySqlConnectionStringBuilder conectionDB = conexaoBanco();
+            MySqlConnection doConection = new MySqlConnection(conectionDB.ToString());
+            try
+            {
+                doConection.Open();
+
+                MySqlCommand commandMysql = doConection.CreateCommand();
+                commandMysql.CommandText = "SELECT * FROM produto WHERE nomeProduto LIKE '"+searchQuery+"%'";
+                MySqlDataReader reader = commandMysql.ExecuteReader();
+
+                gridEstoque.Rows.Clear();
+
+                while (reader.Read())
+                {
+                    DataGridViewRow row = (DataGridViewRow)gridEstoque.Rows[0].Clone();
+                    row.Cells[0].Value = reader.GetInt32(0); //id
+                    row.Cells[1].Value = reader.GetString(1); //prouto
+                    row.Cells[2].Value = reader.GetString(2); //sexo
+                    row.Cells[3].Value = reader.GetString(3); //tamanho
+                    row.Cells[4].Value = reader.GetString(4); //cor
+                    row.Cells[5].Value = reader.GetString(5); //quantidade
+                    gridEstoque.Rows.Add(row); //Add linha na tabela
+                }
+                doConection.Close();
+
+
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("Can't open connetion");
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
